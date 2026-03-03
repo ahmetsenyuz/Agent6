8c8bf2debc1e978c3dc39c32ac497d1c5f2e7912
ahmetsenyuz <89253941+ahmetsenyuz@users.noreply.github.com>
2026-03-03 22:54:10 +0300

    BI-0004: Improve prime checking algorithm for better performance and accuracy

--- PrimeChecker.cs
+++ PrimeChecker.cs
@@ -14,21 +14,21 @@ namespace Agent6
             // Handle edge cases
             if (number <= 1)
                 return false;
-            
+
             if (number <= 3)
                 return true;
-            
+
             // Even numbers greater than 2 are not prime
             if (number % 2 == 0)
                 return false;
-            
+
             // Check for divisors from 3 up to the square root of the number
             for (int i = 3; i * i <= number; i += 2)
             {
                 if (number % i == 0)
                     return false;
             }
-            
+
             return true;
         }
     }
