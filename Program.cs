2de6619d4ee781b12c2161cdd53b5646e1854d0e
ahmetsenyuz <89253941+ahmetsenyuz@users.noreply.github.com>
2026-03-03 22:53:31 +0300

    BI-0002: Update Program.cs to test PrimeChecker implementation

--- Program.cs
+++ Program.cs
@@ -8,6 +8,16 @@ namespace Agent6
         {
             Console.WriteLine("Hello, World!");
             
+            // Test the PrimeChecker
+            Console.WriteLine("Testing PrimeChecker:");
+            int[] testNumbers = { -5, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 17, 25, 29 };
+            
+            foreach (int num in testNumbers)
+            {
+                bool isPrime = PrimeChecker.IsPrime(num);
+                Console.WriteLine($"{num} is {(isPrime ? "" : "not ")}prime");
+            }
+
             // Wait for user input before closing
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
